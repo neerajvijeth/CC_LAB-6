9c3d83e13afcd43597adc012d5f6c6f810612408
neerajvijeth <167126748+neerajvijeth@users.noreply.github.com>
2026-02-28 18:17:16 +0530

    Added Content-Length header to fix HTTP response

--- CC_LAB-6/backend/app.cpp
+++ CC_LAB-6/backend/app.cpp
@@ -42,11 +42,15 @@ int main() {
         int client_fd = accept(server_fd, NULL, NULL);
         if (client_fd < 0) continue;
         
-        // Simple HTTP response
+        // Create response body
+        std::string body = "Served by backend: " + std::string(hostname) + "\n";
+
+        // Proper HTTP response with Content-Length
         std::string response = "HTTP/1.1 200 OK\r\n";
         response += "Content-Type: text/plain\r\n";
+        response += "Content-Length: " + std::to_string(body.length()) + "\r\n";
         response += "Connection: close\r\n\r\n";
-        response += "Served by backend: " + std::string(hostname) + "\n";
+        response += body;
         
         send(client_fd, response.c_str(), response.length(), 0);
         close(client_fd);
